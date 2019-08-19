51be5b58bfce9108fe8970961db73366bbcb5bcd
jinhyung kim <haneu89@gmail.com>
2019-08-19 18:58:07 +0900

    Add a stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import 'random_words.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
 
     return MaterialApp(
       title: 'Welcome to Flutter',
@@ -18,7 +17,7 @@ class MyApp extends StatelessWidget {
           title: Text('Welcome to Flutter'),
         ),
         body: Center(
-          child: Text(wordPair.asPascalCase),
+          child: RandomWords(),
         ),
       ),
     );
